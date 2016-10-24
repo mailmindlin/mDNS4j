6e5807d1b1ab9c6f3b0f1039036167d8bf8c33d2
mailmindlin <liamttlyrox@gmail.com>
2016-10-24 11:10:33 -0400

    Worked on MDNSListener flow

--- src/com/mindlin/mdns/MDNSListener.java
+++ src/com/mindlin/mdns/MDNSListener.java
@@ -6,9 +6,7 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
 
 public class MDNSListener implements Runnable {
 	private static InetAddress lookup(String host, int... addr) {
@@ -32,7 +30,7 @@ public class MDNSListener implements Runnable {
 	protected final InetAddress group;
 	protected final int port;
 	protected final MulticastSocket socket;
-	protected final BlockingQueue<DatagramPacket> queue = new LinkedBlockingQueue<>();
+	protected Consumer<DnsMessage> handler = this::defaultHandler;
 	
 	public MDNSListener() throws SecurityException, IOException {
 		this(MDNS_IP4_ADDR, MDNS_PORT);
@@ -43,41 +41,30 @@ public class MDNSListener implements Runnable {
 		this.group = group;
 		this.port = port;
 		this.socket = new MulticastSocket();
-		this.socket.joinGroup(InetAddress.getByName("224.0.0.251"));
+		this.socket.joinGroup(group);
 	}
 	
-	public void start(ExecutorService executor) {
-		executor.submit(this::run);
-		executor.submit(this::process);
+	public void setHandler(Consumer<DnsMessage> handler) {
+		this.handler = handler;
 	}
 	
 	@Override
 	public void run() {
+		ByteBuffer buf = ByteBuffer.allocate(512);
+		DatagramPacket packet = new DatagramPacket(buf.array(), 0, 512);
 		while (true) {
-			DatagramPacket packet = new DatagramPacket(new byte[512], 0, 512);
+			buf.clear();
+			packet.setLength(512);
 			try {
 				this.socket.receive(packet);
-				queue.put(packet);
 			} catch (Exception e) {
 				e.printStackTrace();
 				break;
 			}
-		}
-	}
-	
-	public void process() {
-		while (true) {
-			DatagramPacket packet;
-			try {
-				packet = queue.take();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				break;
-			}
-			ByteBuffer buf = ByteBuffer.wrap(packet.getData(), packet.getOffset(), packet.getLength());
+			buf.position(packet.getOffset());
 			buf.mark();
-			
-			/*System.out.println(packet.getLength());
+			buf.limit(packet.getLength());
+			System.out.println(buf.remaining());
 			char[] hexChars = "0123456789ABCDEF".toCharArray();
 			for (int i = 0, l = buf.remaining(); i < l; i++) {
 				int b = buf.get() & 0xFF;
@@ -87,35 +74,39 @@ public class MDNSListener implements Runnable {
 				if (i % 32 == 31)
 					System.out.println();
 			}
-			System.out.println();*/
-			
-			buf.reset();
+			System.out.println();
+			buf.flip();
 			DnsMessage message;
 			try {
 				message = DnsMessage.parse(buf);
 			} catch (Exception e) {
 				e.printStackTrace();
-				break;
+				continue;
 			}
-			System.out.println(message);
-			for (int i = 0; i < message.questions.length; i++) {
-				DnsQuery question = message.questions[i];
-				System.out.println("QUERY " + question.getName().toString() + '\t' + question.getQclassStr() + '\t' + question.getType());
-			}
-			for (int i = 0; i < message.answers.length; i++) {
-				DnsRecord answer = message.answers[i];
-				System.out.println("RESP " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType() + '\t' + answer.getData());
-			}
-			for (int i = 0; i < message.authRecords.length; i++) {
-				DnsRecord answer = message.authRecords[i];
-				System.out.println("AUTH " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType());
-			}
-			for (int i = 0; i < message.additionalRecords.length; i++) {
-				DnsRecord answer = message.additionalRecords[i];
-				System.out.println("ADDTL " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType());
-			}
-			System.out.println("DONE");
+			
+			handler.accept(message);
 		}
+	}
+	
+	public void defaultHandler(DnsMessage message) {
+		System.out.println(message);
+		for (int i = 0; i < message.questions.length; i++) {
+			DnsQuery question = message.questions[i];
+			System.out.println("QUERY " + question.getName().toString() + '\t' + question.getQclassStr() + '\t' + question.getType());
+		}
+		for (int i = 0; i < message.answers.length; i++) {
+			DnsRecord answer = message.answers[i];
+			System.out.println("RESP " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType() + '\t' + answer.getData());
+		}
+		for (int i = 0; i < message.authRecords.length; i++) {
+			DnsRecord answer = message.authRecords[i];
+			System.out.println("AUTH " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType());
+		}
+		for (int i = 0; i < message.additionalRecords.length; i++) {
+			DnsRecord answer = message.additionalRecords[i];
+			System.out.println("ADDTL " + answer.getName().toString() + '\t' + answer.getClazz() + '\t' + answer.getType());
+		}
+		System.out.println("DONE");
 	}
 	
 	public void query(String fqdn) throws IOException {
--- src/com/mindlin/mdns/Tester.java
+++ src/com/mindlin/mdns/Tester.java
@@ -10,7 +10,7 @@ public class Tester {
 	public static void main(String...fred) throws SecurityException, IOException, InterruptedException, ExecutionException {
 		MDNSListener listener = new MDNSListener();
 		ExecutorService executor = Executors.newCachedThreadPool();
-		listener.start(executor);
+		executor.submit(listener);
 		Thread.sleep(500);
 		listener.query("raspberrypi.local");
 		listener.query("_services._dns-sd._udp.local");
