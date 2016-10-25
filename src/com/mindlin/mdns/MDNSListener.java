0c08df2a9bd0a3bf91925dee6976548e85833111
mailmindlin <liamttlyrox@gmail.com>
2016-10-25 11:10:41 -0400

    Fixed MDNSListener when socket is externally closed
    Tested with ipconfig/release & ipconfig/renew (Win7, worked)
    Did not test IP address changing
    Need to add close method

--- src/com/mindlin/mdns/MDNSListener.java
+++ src/com/mindlin/mdns/MDNSListener.java
@@ -3,9 +3,14 @@ package com.mindlin.mdns;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 public class MDNSListener implements Runnable {
@@ -24,28 +29,38 @@ public class MDNSListener implements Runnable {
 	/**
 	 * IP4 multicast address.
 	 */
-	public static final InetAddress MDNS_IP4_ADDR = lookup("224.0.0.251", (byte) 224, 0x00, (byte) 0x00, (byte) 251);
+	public static final InetAddress MDNS_IP4_ADDR = lookup("224.0.0.251", 224, 0, 0, 251);
 	public static final InetAddress MDNS_IP6_ADDR = lookup("FF02::FB", 0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFB);
 	
-	protected final InetAddress group;
-	protected final int port;
-	protected final MulticastSocket socket;
+	protected final SocketAddress group;
+	protected final AtomicReference<MulticastSocket> socket;
+	protected final NetworkInterface netIf;
 	protected Consumer<DnsMessage> handler = this::defaultHandler;
 	
 	public MDNSListener() throws SecurityException, IOException {
-		this(MDNS_IP4_ADDR, MDNS_PORT);
+		this(new InetSocketAddress(MDNS_IP4_ADDR, MDNS_PORT), null);
 	}
 	
-	public MDNSListener(InetAddress group, int port) throws SecurityException, IOException {
-		System.out.println("Connecting via group=" + group + ":" + port);
+	public MDNSListener(SocketAddress group, NetworkInterface netIf) throws SecurityException, IOException {
+		System.out.println("Connecting via group=" + group);
 		this.group = group;
-		this.port = port;
-		this.socket = new MulticastSocket();
-		this.socket.joinGroup(group);
+		this.netIf = netIf;
+		this.socket = new AtomicReference<>(null);
+		this.resetSocket();
 	}
 	
 	public void setHandler(Consumer<DnsMessage> handler) {
 		this.handler = handler;
+	}
+	
+	protected void resetSocket() throws IOException {
+		MulticastSocket newSocket = new MulticastSocket();
+		newSocket.setLoopbackMode(false);
+		newSocket.setReuseAddress(true);
+		newSocket.joinGroup(this.group, this.netIf);
+		MulticastSocket oldSocket = this.socket.getAndSet(newSocket);
+		if (oldSocket != null)
+			oldSocket.close();
 	}
 	
 	@Override
@@ -56,7 +71,20 @@ public class MDNSListener implements Runnable {
 			buf.clear();
 			packet.setLength(512);
 			try {
-				this.socket.receive(packet);
+				MulticastSocket socket;
+				//Loop until we get a packet.
+				while (true) {
+					socket = this.socket.get();
+					if (socket == null)
+						//This listener is closed
+						return;
+					try {
+						socket.receive(packet);
+						break;
+					} catch (SocketTimeoutException e) {
+						continue;
+					}
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 				break;
@@ -88,7 +116,7 @@ public class MDNSListener implements Runnable {
 		}
 	}
 	
-	public void defaultHandler(DnsMessage message) {
+	protected void defaultHandler(DnsMessage message) {
 		System.out.println(message);
 		for (int i = 0; i < message.questions.length; i++) {
 			DnsQuery question = message.questions[i];
@@ -116,11 +144,24 @@ public class MDNSListener implements Runnable {
 	}
 	
 	public void sendMessage(DnsMessage message) throws IOException {
-//		System.out.println("Sending " + message);
 		int size = message.getSize();
 		ByteBuffer buf = ByteBuffer.allocate(size);
 		message.writeTo(buf);
-		DatagramPacket packet = new DatagramPacket(buf.array(), 0, size, this.group, this.port);
-		this.socket.send(packet);
+		DatagramPacket packet = new DatagramPacket(buf.array(), 0, size, this.group);
+		this.sendPacket(packet, true);
+	}
+	
+	protected void sendPacket(DatagramPacket packet, boolean retry) throws IOException {
+		MulticastSocket socket = this.socket.get();
+		if (socket == null)
+			return;
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			if (!retry)
+				throw e;
+			this.resetSocket();
+			sendPacket(packet, false);
+		}
 	}
 }
